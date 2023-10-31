085b70a075971ef4f6f6aecbb2371172d2b8106a
Rynte <Podreju.dev@gmail.com>
2023-10-31 17:24:54 +0300

    Добавление классов для Docx

--- HtmlToDocument/Models/Font.cs
+++ HtmlToDocument/Models/Font.cs
@@ -6,12 +6,12 @@
 public class Font
 {
     /// <summary>
-    /// Название шрифта.
-    /// </summary>
-    public string Name { get; set; } = "Times New Roman";
-
-    /// <summary>
     /// Размер шрифта.
     /// </summary>
     public int Size { get; set; } = 14;
+
+    /// <summary>
+    /// Название шрифта.
+    /// </summary>
+    public string Name { get; set; } = "Times New Roman";
 }
